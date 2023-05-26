890533a80977c17715eaf48f073cfc1562e32cd3
geronda94 <iiiliev.igor@gmail.com>
2023-05-26 06:47:08 +0300

    1234

--- view.py
+++ view.py
@@ -1,11 +1,14 @@
-from flask import  render_template, url_for, redirect, session
+from flask import render_template, url_for, redirect, session
 from app import app
 
 menu = {
 	'Каталог товаров':{
-		'Цемент':'index',
-		'Клей':'index',
-		'Песок':'index'
+		'Цемент / Песок / Гравий':'index',
+		'Клей 333 / Шпаклевка':'index',
+		'Декоравтивный Кирпич':'index',
+		'Кирпичи / Фортан / Пеноблоки':'index',
+		'Печные дверцы / Огнеупорные смеси':'index',
+		'Штукарутрная сетка':'index'
 		                },
 	'Связаться с нами':'index',
 	'Достава и разгрузка':'index',
