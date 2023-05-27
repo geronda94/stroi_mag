dc9593ee64b907a23122426c4554c3196460e907
Igor <Iiiliev.igor@gmail.com>
2023-05-27 15:10:33 +0300

    123124

--- static/sql.sql
+++ static/sql.sql
@@ -28,18 +28,20 @@ CREATE TABLE IF NOT EXISTS product (
     name varchar(50),
     description text,
     link text,
-    category_id INT REFERENCES categories (id),
     seller_id INT REFERENCES seller (id),
     price numeric(5, 2),
     price2 numeric(5, 2),
     price3 numeric(5, 2),
     comission numeric(5, 2),
+    order_num INT,
     sold INT
 );
 
 
-INSERT INTO product(name, description, link, category_id, seller_id, price, price2, price3, comission, sold)
-VALUES('Цемент М400', '')
+CREATE TABLE IF NOT EXISTS cat_prod(
+    category_id INT REFERENCES categories (id),
+    product_id INT REFERENCES product(id)
+);
 
 CREATE TABLE IF NOT EXISTS product_img(
     id SERIAL PRIMARY KEY,
@@ -49,8 +51,8 @@ CREATE TABLE IF NOT EXISTS product_img(
 
 
 
-INSERT INTO seller(name, description,address,phone,telegram, email)
-VALUES('self', 'self', 'aval', '77553291', '15444654565', 'iiiliev.igor@gmail.com'),
+INSERT INTO seller(name, description,address,phone,telegram, email) VALUES
+('self', 'self', 'aval', '77553291', '15444654565', 'iiiliev.igor@gmail.com'),
 ('Аваль', 'Цемент Песок Гравий', 'Мегатранс', '77782349', '15444654565', 'iiiliev.igor@gmail.com');
 
 
@@ -61,3 +63,13 @@ INSERT INTO categories(name, description) VALUES
 ('Плитка, бордюры и прочие изделия', 'Тротуарная плитка, Коньки и крышки для заборов, Бордюры, Водостоки'),
 ('Камины и печи', 'Дверцы печные, Жаровые Решетки, Дымоходные затворы, Клей огнеупорный'),
 ('Клей, шпатлевка и сетка', 'Клей 333, Шпатлевка первичная и вторичная. Армирующая сетка 1*2м с ячейкой: 100*100мм, 150*150мм.');
+
+
+INSERT INTO product(name, description, link, seller_id, price, price2, price3, comission, sold, order_num)VALUES
+('Цемент М 400', 'Цемент марки 400 рыбницкого завода', 'cement-m400', '2','85', '84','83','0','0','1'),
+('Цемент М 500', 'Цемент марки 500 рыбницкого завода', 'cement-m500', '2','90', '89','88','0','0','2'),
+('Цемент М 550', 'Цемент марки 550 рыбницкого завода', 'cement-m550', '2','90', '89','88','0','0',NULL),
+('Песок сеянный', 'Цемент сеянный с содержанием глины подходит для кладки и штукатурки', 'pesok-1', '2','13', '0','0','0','0',NULL),
+('Песок Мытый', 'Цемент мытый, без глины', 'pesok-2', '2','14', '0','0','0','0','3'),
+('Гравий', 'Гравий среднего размера', 'gravii', '2','18', '0','0','0','0','4'),
+('Мелуза', 'Мелуза или известковый отсев. Молотая, отлично подходит для добавки в штукатурные смеси, фасадной отделки и для добавления в корм животных', 'meluza', '2','25', '0','0','0','0','5');
