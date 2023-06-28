19e2c9b70e9bc801c465835dee9a8380ce6bec5a
geronda94 <iiiliev.igor@gmail.com>
2023-06-28 21:40:37 +0300

    1234

--- view.py
+++ view.py
@@ -36,7 +36,7 @@ def index():
 
 @app.route('/json')
 def json_try():
-	return json.dump({'hello':'world'}),200
+	return json.dumps({'status':200, 'time':datetime.now().strftime('%H:%M')}),200
 
 
 @app.route('/category/<cat>')
